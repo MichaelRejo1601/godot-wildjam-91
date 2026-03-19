fc85e48ad1e06f0c44a9041259601d615dba75f5
Michael Rejo <ninjapenguin1601@gmail.com>
2026-03-19 01:08:28 -0400

    add Raider animation to Player

--- scenes/Player/PlayerController.gd
+++ scenes/Player/PlayerController.gd
@@ -1,25 +1,54 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 70.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+enum PlayerStates {
+	WALK_LEFT,
+	WALK_RIGHT,
+	IDLE_LEFT,
+	IDLE_RIGHT,
+}
+
+var current_state: PlayerStates = PlayerStates.IDLE_RIGHT
+var facing_left := false
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	
-	# Get the input directionX and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var directionX := Input.get_axis("move_left", "move_right")
-	var directionY := Input.get_axis("move_up", "move_down")
+func _ready() -> void:
+	update_animation(Vector2.ZERO)
 
-	if directionX:
-		velocity.x = directionX * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	if directionY:
-		velocity.y = directionY * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+func _physics_process(_delta: float) -> void:
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * SPEED
 
+	update_animation(input_direction)
 	move_and_slide()
+
+
+func update_animation(input_direction: Vector2) -> void:
+	if input_direction.x < 0.0:
+		facing_left = true
+	elif input_direction.x > 0.0:
+		facing_left = false
+
+	var is_moving := input_direction.length_squared() > 0.0
+	if is_moving:
+		current_state = PlayerStates.WALK_LEFT if facing_left else PlayerStates.WALK_RIGHT
+	else:
+		current_state = PlayerStates.IDLE_LEFT if facing_left else PlayerStates.IDLE_RIGHT
+
+	var animation_name := ""
+	match current_state:
+		PlayerStates.WALK_LEFT:
+			animation_name = "WalkLeft"
+		PlayerStates.WALK_RIGHT:
+			animation_name = "WalkRight"
+		PlayerStates.IDLE_LEFT:
+			animation_name = "IdleLeft"
+		PlayerStates.IDLE_RIGHT:
+			animation_name = "IdleRight"
+
+	if animated_sprite.animation != animation_name or not animated_sprite.is_playing():
+		animated_sprite.play(animation_name)
