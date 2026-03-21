bc445626541d2fc30a9919baceb26b645f44b8b6
Michael Rejo <ninjapenguin1601@gmail.com>
2026-03-20 22:18:33 -0400

    Change menu name

--- addons/maaacks_game_template/base/nodes/labels/config_name_label.gd
+++ addons/maaacks_game_template/base/nodes/labels/config_name_label.gd
@@ -2,7 +2,7 @@
 extends Label
 ## Displays the value of `application/config/name`, set in project settings.
 
-const NO_NAME_STRING : String = "Title"
+const NO_NAME_STRING : String = "Death Below"
 
 ## If true, update the title when ready.
 @export var auto_update : bool = true
